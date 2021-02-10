cfa899a7ef9b07aa2a55116e483199bf4f3fe722
Mazur Valeria <mazurvaleria@gmail.com>
2021-02-10 13:04:44 +0200

    calculator

--- Homework/home0802.php
+++ Homework/home0802.php
@@ -1,19 +1,14 @@
 <?php
 
-if (isset($_POST['submit'])) {
-    $result1 = $_POST['number1'];
-    $result2 = $_POST['number2'];
-    $operator = $_POST['operator'];
-    switch ($operator) {
+    $result1 = readLine ('Insert number1');
+    $result2 = readLine ('Insert number2');
+    $operator = readLine ('Insert operator');
 
-        case 'Add':
-            echo $result1 + $result2;
-        case 'Subtract':
-            echo $result1 - $result2;
-        case 'Multiply':
-            echo $result1 * $result2;
-        case 'Divide':
-            echo $result1 / $result2;
-    }
-}
-
+    if ($operator == '+') {
+            echo $result1 + $result2;}
+    if ($operator == '-') {
+            echo $result1 - $result2;}
+    if ($operator == '*') {
+            echo $result1 * $result2;}
+    if ($operator == '/') {
+            echo $result1 / $result2;}
