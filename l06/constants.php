60d8ac55bad93c2929944df99373d2c74d4cb791
Mazur Valeria <mazurvaleria@gmail.com>
2021-02-11 21:01:30 +0200

    constants

---
+++ l06/constants.php
@@ -0,0 +1,21 @@
+<?php
+
+define('APP_VERSION','3.0.1');
+//define('APP_VERSION','3.0.2');// ne mojem ustanoviti drugoe znacenie constanti esli mi eio ustanovili
+
+echo APP_VERSION, PHP_EOL;
+
+const POWER = '220V';
+//const POWER = '300V';
+
+echo POWER, PHP_EOL;
+
+const MY_COEFICIENT = 4;
+$salary = 2000;
+echo $salary * MY_COEFICIENT, PHP_EOL;
+
+echo __DIR__,PHP_EOL;
+echo __FILE__, PHP_EOL;
+echo __LINE__, PHP_EOL;
+
+
