d5c62196a13f360fe9b5a9de603934bc0f221a08
Mazur Valeria <mazurvaleria@gmail.com>
2021-02-18 00:07:43 +0200

    calculator

--- Homework/home1502.php
+++ Homework/home1502.php
@@ -9,3 +9,21 @@ if($x == 2) {
 if($x == 3) {
     echo 3, PHP_EOL;
 }
+
+
+$firstFigure = readLine('Insert first figure:');
+$secondFigure = readLine('Insert second figure:');
+$operator = readLine('Insert operator:');
+
+    if ($operator == '+') {
+        echo "Answer is: " .$firstFigure +$secondFigure, PHP_EOL;
+    }
+    if ($operator == '-') {
+        echo "Answer is: " .$firstFigure - $secondFigure, PHP_EOL;
+    }
+    if ($operator == '*') {
+        echo "Answer is: " .$firstFigure * $secondFigure, PHP_EOL;
+    }
+    if ($operator == '/') {
+        echo "Answer is: " .$firstFigure / $secondFigure, PHP_EOL;
+    }
