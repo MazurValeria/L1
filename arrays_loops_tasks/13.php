e194ad910f9bbacc22d814aba83897dffb07ed4f
Mazur Valeria <mazurvaleria@gmail.com>
2021-02-24 23:20:41 +0200

    13

---
+++ arrays_loops_tasks/13.php
@@ -0,0 +1,10 @@
+<?php
+$eol = PHP_SAPI === 'cli' ? PHP_EOL : '<br>';
+
+for ($col = 2; $col <= 9; $col++) {
+    for ($row = 1; $row <= 10; $row++){
+        $result = $col * $row;
+        echo "{$col} x {$row} = {$result}", $eol;
+    }
+    echo $eol;
+}
