fcd94b1d41aefe3cca1d82935a4fb6b916d4ebb6
Mazur Valeria <mazurvaleria@gmail.com>
2021-02-25 00:07:36 +0200

    14 neponeala

---
+++ arrays_loops_tasks/14.php
@@ -0,0 +1,10 @@
+<?php
+$eol = PHP_SAPI === 'cli' ? PHP_EOL : '<br>';
+
+$arr = [4, 2, 5, 19, 13, 0, 10];
+    foreach ($arr as $e) {
+        if ($e == 2 || $e == 3 || $e == 4) {
+            echo 'Есть!', $eol;
+        }
+    }
+    echo 'Нет!', $eol;
