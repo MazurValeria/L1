677c93e2ecb9811e0fb2b74edba5f4890669d935
Mazur Valeria <mazurvaleria@gmail.com>
2021-03-02 17:48:08 +0200

    draft

---
+++ Homework/home0103.php
@@ -0,0 +1,69 @@
+<?php
+
+$menuArray = [
+    [
+        'title' => 'Home',
+        'link' => '/',
+    ],
+    [
+        'title' => 'About',
+        'link' => '/about-us'
+    ],
+    [
+        'title' => 'Products',
+        'link' => '/catalog',
+        'children' => [
+            [
+                'title' => 'Phones',
+                'link' => '/catalog/phones'
+            ],
+            [
+                'title' => 'TV',
+                'link' => '/catalog/tv'
+            ],
+            [
+                'title' => 'MP3',
+                'link' => '/catalog/mp3'
+            ],
+        ],
+    ],
+    [
+        'title' => 'Contacts',
+        'link' => '/contacts',
+    ],
+];
+
+
+function recursiveMenu($menu)
+{
+    $html = '<ul>';
+    foreach ($menu as $item) {
+
+        if (is_array($item)) {
+            $html .= recursiveMenu($item);
+        } else {
+            $html .= "<li><a href='{$menu['link']}'>{$menu['title']}</a></li>";
+        }
+    }
+    $html .= '</ul>';
+    return $html;
+}
+
+echo recursiveMenu($menuArray);
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+</body>
+</html>
+
