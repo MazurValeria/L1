bd77b2b25ef28f58e0ad94494d42534cc4cf204c
Mazur Valeria <mazurvaleria@gmail.com>
2021-03-11 10:53:32 +0200

    add comment

--- l12/contacts/add-comment.php
+++ l12/contacts/add-comment.php
@@ -1,21 +1,20 @@
 <?php
 
-//var_dump($_REQUEST, $_POST, $_GET);// get budet pustoi, post i request odinakovie
+$file = __DIR__ . '/storage/' . date('Y-m-d') . '.json';
 
-$file =__DIR__ . '/storage/' . date('Y-m-d') . '.json';
+//$storage = fopen($file, 'wb');
+//
+//fclose($storage);
 
-//Read data begin
+// Read data begin
 $jsonData = file_get_contents($file);
 $storage = json_decode($jsonData, true);
-//Read data end
+// Read data end
 
-
-//Write data begin
+// Write data begin
 $data = $_POST;
 $data['time'] = time();
-
 $storage[] = $data;
-
 $jsonData = json_encode($storage);
 
 file_put_contents($file, $jsonData);// sozdati file kakoi i cito polojiti v nego
@@ -29,7 +28,14 @@ header('Location: index.php');//perehod na druguiu stranitzu, ukaz otnositelinii
 
 
 
-//$storage = fopen($file, 'wb');
-//
-//fclose($storage);
+
+
+
+
+
+
+
+
+
+
 
